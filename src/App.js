3010be8182abc4915cc5fcbdb67300263d67af2f
Ernie Park <eipark@ernies-mbp.lan>
2022-08-29 11:17:48 -0400

    init with create-react-app and chakra
    npx create-react-app my-app --template @chakra-ui

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Edit <Code fontSize="xl">src/App.js</Code> and save to reload. What'su
             </Text>
             <Link
               color="teal.500"
