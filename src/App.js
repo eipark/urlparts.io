3ce8aae81443ae5fcd28dd6e6a9c84464d8fc0b1
Ernie Park <ernestipark@gmail.com>
2022-08-31 15:04:12 -0400

    child app and nav

--- src/App.js
+++ src/App.js
@@ -1,70 +1,10 @@
 import * as React from 'react'
-import {
-  Avatar,
-  Box,
-  Button,
-  ButtonGroup,
-  Container,
-  Flex,
-  HStack,
-  IconButton,
-  useBreakpointValue,
-  useColorModeValue,
-} from '@chakra-ui/react'
-import { FiHelpCircle, FiMenu, FiSearch, FiSettings } from 'react-icons/fi'
-import { Logo } from './Logo'
+import Nav from './Nav';
 
 const App = () => {
-//  const isDesktop = useBreakpointValue({
-//    base: false,
-//    lg: true,
-//  })
-  const isDesktop = true;
   return (
-    <Box
-      as="section"
-      pb={{
-        base: '12',
-        md: '24',
-      }}
-    >
-      <Box as="nav" bg="bg-surface" boxShadow={useColorModeValue('sm', 'sm-dark')}>
-        <Container
-          py={{
-            base: '3',
-            lg: '4',
-          }}
-        >
-          <Flex justify="space-between">
-            <HStack spacing="5">
-              <Logo />
-              {isDesktop && (
-                <ButtonGroup variant="ghost" spacing="1">
-                  <Button>Home</Button>
-                  <Button aria-current="page">Dashboard</Button>
-                  <Button>Users</Button>
-                </ButtonGroup>
-              )}
-            </HStack>
-            {isDesktop ? (
-              <HStack spacing="4">
-                <ButtonGroup variant="ghost" spacing="1">
-                  <IconButton icon={<FiSearch fontSize="1.25rem" />} aria-label="Search" />
-                  <IconButton icon={<FiSettings fontSize="1.25rem" />} aria-label="Settings" />
-                  <IconButton icon={<FiHelpCircle fontSize="1.25rem" />} aria-label="Help Center" />
-                </ButtonGroup>
-              </HStack>
-            ) : (
-              <IconButton
-                variant="ghost"
-                icon={<FiMenu fontSize="1.25rem" />}
-                aria-label="Open Menu"
-              />
-            )}
-          </Flex>
-        </Container>
-      </Box>
-    </Box>
-  )
+    <Nav />
+  );
 }
+
 export default App;
